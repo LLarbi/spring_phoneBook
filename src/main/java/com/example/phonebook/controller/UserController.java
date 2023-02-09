578890b2718b678dccdd27dcd119886294729f45
LLarbi <larbi_lachgare@hotmail.fr>
2023-02-09 21:27:36 +0100

    edit user

--- src/main/java/com/example/phonebook/controller/UserController.java
+++ src/main/java/com/example/phonebook/controller/UserController.java
@@ -5,12 +5,8 @@ import com.example.phonebook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 @RequestMapping(path="/users")
@@ -19,15 +15,28 @@ public class UserController {
     private UserService userService;
 
     @GetMapping(path="/register")
-    public String FormCreateUser(Model model) {
+    public String createUserForm(Model model) {
         model.addAttribute("user",new User());
         return "add-user";
     }
 
     @PostMapping(path="/register")
-    public String CreateUser(@ModelAttribute User user){
+    public String createUser(@ModelAttribute User user){
         userService.create(user);
         return "redirect:/users/register";
+    }
+
+    @GetMapping(path="/edit/{id}")
+    public String editUserForm(Model model, @PathVariable Integer id) {
+        User user = userService.findById(id);
+        model.addAttribute("user",user);
+        return "edit-user";
+    }
+
+    @PostMapping(path="/edit/{id}")
+    public String editUser(User user){
+        userService.update(user);
+        return "redirect:/contacts/all";
     }
 
 }
--- src/main/java/com/example/phonebook/service/UserService.java
+++ src/main/java/com/example/phonebook/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.phonebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -12,6 +14,18 @@ public class UserService {
     private UserRepository userRepository;
 
     public void create(User user){
+        userRepository.save(user);
+    }
+
+    public User findById(Integer id) {
+        Optional<User> userOpt = userRepository.findById(id);
+        if(userOpt.isPresent()){
+            return userOpt.get();
+        }
+        return new User();
+    }
+
+    public void update(User user) {
         userRepository.save(user);
     }
 }
